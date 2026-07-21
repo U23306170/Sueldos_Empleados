4d26e3516ca31bddb0160a62c831cab9b0b865d3
U23306170 <alexisrivera.bajun23@gmail.com>
2026-07-20 21:22:52 -0500

    nuevo commit

--- src/controlador/AreaControlador.java
+++ src/controlador/AreaControlador.java
@@ -3,17 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controlador;
+
 import dao.AreaDAO;
 import dao.AreaDAOImpl;
 import java.util.List;
 import modelo.Area;
+
 /**
  *
  * @author User
  */
 public class AreaControlador {
+
     private final AreaDAO areaDAO;
-    public AreaControlador() { this(new AreaDAOImpl()); }
-    public AreaControlador(AreaDAO areaDAO) { this.areaDAO = areaDAO; }
-    public List<Area> listar() { return areaDAO.listar(); }
+
+    public AreaControlador() {
+        this(new AreaDAOImpl());
+    }
+
+    public AreaControlador(AreaDAO areaDAO) {
+        this.areaDAO = areaDAO;
+    }
+
+    public List<Area> listar() {
+        return areaDAO.listar();
+    }
 }
